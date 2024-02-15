e77d2ed31248530552084b00ddcec54b97297871
calculadoraSimples <victortadashisaitobarra@gmail.com>
2024-02-15 08:12:32 -0300

    Aula calculadora simples

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,21 @@ export class HomePage {
 
   constructor() {}
 
+  n1 = '';
+  n2 = '';
+  res = 0;
+
+  somar(){
+    this.res = parseFloat(this.n1)+parseFloat(this.n2);
+  }
+  subtrair(){
+    this.res = parseFloat(this.n1)-parseFloat(this.n2);
+  }
+  multiplicar(){
+    this.res = parseFloat(this.n1)*parseFloat(this.n2);
+  }
+  dividir(){
+    this.res = parseFloat(this.n1)/parseFloat(this.n2);
+  }
+
 }
